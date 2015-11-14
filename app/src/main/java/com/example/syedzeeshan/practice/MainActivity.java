0ed206f23d255d3114da0afa040ee978ddb9d2f2
zeeshan7244 <k102169@nu.edu.pk>
2015-11-14 16:36:18 +0500

    Android greeting project

---
+++ app/src/main/java/com/example/syedzeeshan/practice/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.syedzeeshan.practice;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    Button greetButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+      // ________________________________________________________________
+        greetButton = (Button) findViewById(R.id.greetButton);
+        //getting the reference of the button in this method
+        greetButton.setOnClickListener(this);
+        // activating the response
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+
+
+        EditText friendName = (EditText) findViewById(R.id.friendName);
+
+        // getting the reference of the edit text for extracting the typed Friend Name
+
+        String name  = friendName.getText().toString();
+
+        switch(view.getId()){
+
+          case R.id.greetButton:
+
+
+              Intent in = new Intent(this , ShowMessage.class);
+
+              in.putExtra("message" , "Good Day Sir " + name   + " !");
+
+
+              startActivity(in);
+
+              break;
+
+           default:
+        }
+
+    }
+}
